32f5b0757bdd5b9c1453dd4b8b8f3c15c0c18d94
SpanishBlogs <SpanishBlogs@users.noreply.github.com>
2017-05-27 23:53:47 +0200

    Create Spanish.lua

---
+++ bwhitelist/Spanish.lua
@@ -0,0 +1,98 @@
+--[[
+	
+	BWhitelist Spanish
+
+    Made by : Shira115 " STEAM_0:0:90539200  "
+
+ 
+###########################################################
+
+               SPANISH VERSION OF 
+              _     _                     
+             | |   | |                    
+             | |__ | |     ___   __ _ ___ 
+             | '_ \| |    / _ \ / _` / __|
+             | |_) | |___| (_) | (_| \__ \
+             |_.__/|______\___/ \__, |___/
+                                 __/ |    
+                                |___/     
+ 
+###########################################################
+ 
+
+]]
+
+{
+	permission_failure = "Actualmente no tienes los permisos para hacer esto",
+	disabled_whitelist = "La lista blanca para estos trabajos esta deshabilitada.",
+	no_whitelist_permission = "Tú no tienes los permisos para editar la lista blanca.",
+	search_jobs = "Buscar trabajos...",
+	steamid_finder = "Buscador de SteamIDs",
+	type = "Tipo",
+	value = "Valor",
+	team_name = "Nombre",
+	select_job = "Selecciona un trabajo de la izquierda...",
+	enable_whitelist = "¿Activar lista blanca?",
+	enabling_whitelist = "Activando lista blanca...",
+	add_to_whitelist = "Añadir a la lista blanca",
+	disable_whitelist = "Desactivar lista blanca",
+	clear_whitelist = "Limpiar lista blanca",
+	search = "Buscar...",
+	no_data = "¡Sin informacion!",
+	retrieving = "Recuperando..",
+	admin_mode = "Modo administrador",
+	admin_tab = "Administrador",
+	jobs_tab = "Trabajos",
+	players_tab = "Jugadores",
+	default_team = "Este es el equipo por defecto. You no puedes añadir a la lista blanca el equipo predeterminado.",
+	remove_from_whitelist = "Quitar de la lista blanca",
+	player_name = "Nombre",
+	remove_from_whitelist_selected_tooltip = "Borrar este usuario de la lista blanca",
+	add_to_whitelist_selected_tooltip = "Añadir el usuario seleccionado a la lista blanca",
+	clear_whitelist_tooltip = "[Solo administrador] Limpia la lista blanca. Todas las SteamIDs y los usuarios han sido removidos.",
+	disable_whitelist_tooltip = "Desactivas la lista blanca. Usuarios pueden acceder a los trabajos libremente.",
+	add_to_whitelist_tooltip = "Adds a SteamID, SteamID64 or usergroup to the whitelist for this job.",
+	remove_from_whitelist_tooltip = "Elimina la siguiente SteamID, SteamID64 o Grupo del usuario de la lista blanca de este trabajo",
+	in_jobs_file_tooltip = "Tu no puedes desactivar esta lista blanca porque eso esta BWhitelist activado en tu archivos de trabajos de DarkRP",
+	add_steamid = "Añadir SteamID/64",
+	add_usergroup = "Añadir grupo del usuario",
+	add_usergroup_modal = "Escribe el grupo del usuario que quieres añadir abajo.",
+	add_steamid_modal = "Pega la SteamID/SteamID64 que quieres añadir abajo.\nPorfavor recuerda que hay una manera más fácil de hacer esto, vaya a la \"Jugador\" TAB en el menu!",
+	error = "Error",
+	not_a_steamid_or_64 = "Esto no es una SteamID o SteamID64!",
+	already_exists = "¡Esto ya esta en la lista blanca!",
+	doesnt_exist = "Esto no existe en la lista blanca",
+	copy = "Copiar",
+	copy_tooltip = "Copia el grupo seleccionado o SteamID.",
+	player_left = "Perdon, parece que el jugador se ha ido del servidor.",
+	no_enabled_whitelists = "¡No hay lista blanca a la que puedas acceder!",
+	import_from_mayoz = "Importar las listas blancas del Mayoz",
+	import_from_nordahl = "Importar de Nordahl's Whitelisting System",
+	import_from_old_bwhitelist = "Importar de antigua BWhitelist",
+	view = "Ver",
+	no_import = "Parece que no pudimos encontrar los archivos / los archivos están dañados los que requieres para importar.",
+	import_from_old_bwhitelist_tooltip = "Si podemos encontrar cualquiera de los antiguos archivos de BWhitelist en su servidor, los convertiremos en los nuevos archivos.",
+	import_from_mayoz_tooltip = "Si nosotros podemos encontrar algun Mayoz's Whitelisting System's archivo en tu servidor, nosotros lo convertiremos en archivos de BWhitelist.",
+	import_from_nordahl_tooltip = "Si podemos encontrar cualquiera de los archivos del sistema Whitelisting de Nordahl en su servidor, los convertiremos en archivos de lista blanca.",
+	disable_all_whitelists = "Todos los trabajos tienen su lista blanca desativada. Toda la informacion sigue",
+	enable_all_whitelists = "Todos los trabajos tendrán sus listas blancas habilitadas.",
+	steamprofile = "Perfil de Steam",
+	confirm = "Confirmar",
+	are_you_sure = "¿Estas seguro de hacer esto?",
+	yes = "Si",
+	cancel = "Cancelar",
+	reset_everything = "Restaurar todo",
+	reset_everything_tooltip = "ADVERTENCIA: Esto borrará todas los archivos de la lista y listas blancas activadas.",
+	go = "Vamos",
+	customcheck_team = "Este equipo tiene un customCheck en el. Esto anula la BWhitelist.\n\nPorfavor cambia\"customCheck\" a \"BWhitelist_customCheck\"\nen los archivos de los trabajos para tu customCheck para funcionar con BWhitelist.",
+	removed_from_whitelist = "Tu has sido removido de la lista blanca por \"%s\"",
+	added_to_whitelist = "Tu has sido añadido a la lista blanca por \"%s\"",
+	disable_all_whitelists = "Desactivar todas las listas blanca",
+	enable_all_whitelists = "Activar todas las listas blancas",
+	player_left = "El jugador se ha ido.",
+	no_actions_available = "Ninguna accion disponible para este jugador.",
+	remove_from_all_whitelists = "Removido de todas las listas blanca",
+	add_to_all_whitelists = "Añadir a todas las listas blancas",
+	clear_unknown_jobs = "Limpiar trabajos desconocidos",
+	clear_unknown_jobs_tooltip = "Si hay un archivo de la lista blanca en la base de datos relacionado con un trabajo que ya no existe, todo se borrará con este botón. Los datos de la lista blanca de ese trabajo se mantendrán en caso de que lo necesite en el futuro.",
+}
